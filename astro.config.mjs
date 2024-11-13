2a2516c8258b66ed9df7c4bc08ef335dac88cd2b
D4nk <asta.fcb@yahoo.com>
2024-11-13 20:55:33 +0100

    RETRY DEPLOY VERCEL

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,10 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 import vercel from '@astrojs/vercel/serverless';
 
-import Analytics from '@vercel/analytics/astro';
-
-import SpeedInsights from "@vercel/speed-insights/astro";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
